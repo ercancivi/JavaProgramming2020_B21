d7de13cb570b8f3fba173078b182f16e57eec282
Ercan Civi <ercancivi@gmail.com>
2020-11-30 16:06:23 -0600

    day34 11-30-2020 ArrayList Continue - Collection Utility - BulkOperators - removeIf(lambda)

---
+++ src/day34_11_30_2020_ArrayListContinue_CollectionUtility_BulkOperators_removeIf_lambda/FrequencyOfCharacters2.java
@@ -0,0 +1,32 @@
+package day34_11_30_2020_ArrayListContinue_CollectionUtility_BulkOperators_removeIf_lambda;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class FrequencyOfCharacters2 {
+
+    public static void main(String[] args) {
+        String str = "AAABBCDDDDEEE";
+        String result = "";             // A3B2C1D4E3
+
+        ArrayList<Character> list = new ArrayList<>();
+
+        for(char each : str.toCharArray() )  list.add(each);
+
+
+        for(char each : list ){
+            int f = Collections.frequency(list, each);
+            if(result.contains(""+each)){
+                continue;
+            }
+            result +=  each+"" + f;
+        }
+
+        System.out.println(result);
+
+
+
+    }
+
+
+}
