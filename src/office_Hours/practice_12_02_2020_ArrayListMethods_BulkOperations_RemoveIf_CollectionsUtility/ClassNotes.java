91e30f08ee3fbe1105f39641386c6173e2eee549
Ercan Civi <ercancivi@gmail.com>
2020-12-02 15:29:29 -0600

    practice 12-02-2020 Array List Methods - Bulk Operations-removeIf - Collections Utility

---
+++ src/office_Hours/practice_12_02_2020_ArrayListMethods_BulkOperations_RemoveIf_CollectionsUtility/ClassNotes.java
@@ -0,0 +1,37 @@
+package office_Hours.practice_12_02_2020_ArrayListMethods_BulkOperations_RemoveIf_CollectionsUtility;
+
+public class ClassNotes {
+
+    public static void main(String[] args) {
+
+          /*    12/02/2020
+                Topics: ArrayList methods:
+                                Bulk Operations
+                                removeIf
+                        Collections Utility
+                package name: Practice_12_02_2020
+                3 Data Structure:
+                        1. Array
+                        2. Collection  ==> Iterable
+                        3. Map
+                ArrayList methods:
+                        get
+                        set
+                        contains
+                        clear
+                        equals
+                        addAll(CollectionType)
+                        removeAll(CollectionType)
+                        retainAll(CollectionType)
+                        removeIf
+                Collections utility:
+                        sort
+                        swap
+                        frequency
+                        max
+                        min
+          */
+
+    }
+
+}
